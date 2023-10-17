81d6607907d1808240a1b4f3eec81e72a51cadec
FThinker <filippotaiani05@gmail.com>
2023-10-17 10:33:30 +0200

    Add test file for requests

---
+++ Backend/request.js
@@ -0,0 +1,8 @@
+/*const options = {
+  hostname: 'https://i.instagram.com/api/v1/users/web_profile_info/?username=<USERNAME>.', //!!!Sostituire USERNAME!!!
+  headers: { 'User-Agent': 'Instagram 76.0.0.15.395 Android (24/7.0; 640dpi; 1440x2560; samsung; SM-G930F; herolte; samsungexynos8890; en_US; 138226743)' }
+};
+
+http.get(options, function(res) {
+  res.on('data', function(chunk) { console.log(chunk) });
+});*/
