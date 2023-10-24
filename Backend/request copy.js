6c04ae4c3b58db3e22f3eb9f172e45695a325142
FThinker <filippotaiani05@gmail.com>
2023-10-24 10:29:29 +0200

    Fixes undefined bug, MQTT works correctly now

--- Backend/request copy.js
+++ Backend/request copy.js
@@ -35,6 +35,17 @@ const client = mqtt.connect(connectUrl, {
 let oldFollowerCount = 0; // This variable is used to check if the follower count has increased/decreased
 let output;
 
+//MQTT Connection
+const topic = 'IFC-Backend/1'
+
+client.on('connect', () => {
+  console.log('Connected')
+  client.subscribe([topic], () => { // Subscribes to specified topic
+    console.log(`Subscribed to topic '${topic}'`)
+  })
+
+})
+
 function fetchFollowerCount() {
   let result = 0;
   let req = https.request(options, function (res) {
@@ -52,10 +63,15 @@ function fetchFollowerCount() {
           let followerCount = jsonData.data.user.edge_followed_by.count;
 
           if (followerCount != oldFollowerCount) {
-            console.log("Follower number: " + followerCount);
             oldFollowerCount = followerCount;
-            result = String(followerCount);
-            console.log("Result: " + result);
+            output = String(followerCount);
+
+            console.log("publishing message..." + " [" + output + "]");
+            client.publish(topic, output, { qos: 0, retain: false }, (error) => { // Sends message to the broker
+              if (error) {
+                console.error(error)
+              }
+            })
           }
           else {
             console.log("Follower number hasn't changed");
@@ -81,33 +97,13 @@ function fetchFollowerCount() {
   return result;
 }
 
-output = fetchFollowerCount(); // This is heavily unoptimized
-  // for it to be sent, this variable has to be a string
+// This is heavily unoptimized
+// for it to be sent, this variable has to be a string
 
-//MQTT Connection
-const topic = 'IFC-Backend/1'
-
-client.on('connect', () => {
-  console.log('Connected')
-  client.subscribe([topic], () => { // Subscribes to specified topic
-    console.log(`Subscribed to topic '${topic}'`)
-  })
-
-  console.log("Output: " + output);
-
-  console.log("publishing message...");
-  client.publish(topic, output, { qos: 0, retain: false }, (error) => { // Sends message to the broker
-    if (error) {
-      console.error(error)
-    }
-  })
-
-})
-/*
 client.on('message', (topic, payload) => {
   console.log('Received Message:', topic, payload.toString())
-})*/
+})
 
 
-  // Runs the function every 10 seconds
-  setInterval(fetchFollowerCount, 10000);
+// Runs the function every 10 seconds
+setInterval(fetchFollowerCount, 10000);
