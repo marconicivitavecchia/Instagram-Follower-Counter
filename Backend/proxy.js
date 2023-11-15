5e737d2da4fba38c665bca58295299e2c8bd46da
Prinxino <prinino4@outlook.com>
2023-11-15 19:01:15 +0100

    Updated comments
    In-Depth comments has been added

--- Backend/proxy.js
+++ Backend/proxy.js
@@ -1,24 +1,47 @@
+// Import required modules
 const axios = require('axios');
-const cheerio = require('cheerio'); //required for funny parsing
+const cheerio = require('cheerio');
 const { HttpsProxyAgent } = require('https-proxy-agent');
 
-const url = 'https://instagram.com/iismarconicivitavecchia/'; //destination address
-const proxyAgent = new HttpsProxyAgent(
-  'http://'); //proxy
+// Target URL
+const url = 'https://instagram.com/iismarconicivitavecchia/';
 
+// Proxy configuration with correct credentials
+const proxyCredentials = {
+  username: 'your-username', // Replace with your proxy username
+  password: 'your-password', // Replace with your proxy password
+};
+
+const proxyAgent = new HttpsProxyAgent({
+  protocol: 'https:',
+  host: 'gate.smartproxy.com',
+  port: 10000,
+  auth: `${proxyCredentials.username}:${proxyCredentials.password}`,
+});
+
+// Make an HTTP GET request to the target URL using Axios and the proxy agent
 axios
   .get(url, {
     httpsAgent: proxyAgent,
   })
   .then((response) => {
+    // Extract HTML content from the response
     const html = response.data;
-    const $ = cheerio.load(html);
-    const followersString = $('meta[property="og:description"]').attr('content'); //gets <meta> element with "og:description" property
-    const match = followersString.match(/(\d+) Followers/); //not sure what this does
-    const followersCount = match ? parseInt(match[1], 10) : null; //this as well
 
-    console.log(followersCount);
+    // Load HTML content into Cheerio for easier manipulation
+    const $ = cheerio.load(html);
+
+    // Extract the followers count from the meta tag in the HTML
+    const followersString = $('meta[property="og:description"]').attr('content');
+    
+    // Use a regular expression to extract the numeric followers count
+    const match = followersString.match(/(\d+) Followers/);
+    const followersCount = match ? parseInt(match[1], 10) : null;
+
+    // Log the followers count to the console
+    console.log('Followers count:', followersCount);
   })
   .catch((error) => {
-    console.error('Errore durante la richiesta:', error);
+    // Log any errors that occur during the request
+    console.error('Error during the request:', error);
   });
