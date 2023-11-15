e929232b87a08cdff61cb98d4274f285defe5c07
Gabriele Italia <92986550+Kylessh@users.noreply.github.com>
2023-11-15 21:25:12 +0000

    Added a loop to execute the request every second.

--- Backend/proxy.js
+++ Backend/proxy.js
@@ -7,39 +7,45 @@ const { HttpsProxyAgent } = require('https-proxy-agent'); // HTTP proxy agent
 const url = 'https://instagram.com/iismarconicivitavecchia/';
 
 // Creating an HTTPS proxy agent for Axios
-const proxyAgent = new HttpsProxyAgent('http://your-username:your-password@gate.smartproxy.com:10003'); // Replace with your own proxy details
+const proxyAgent = new HttpsProxyAgent(''); // Replace with your own proxy details
 
-// Making a GET request using Axios
-axios
-  .get(url, { httpsAgent: proxyAgent }) // Sending a GET request to the Instagram profile URL using Axios and the specified proxy agent
-  .then((response) => {
-    // Parsing HTML content using Cheerio
-    const html = response.data; // Extracting the HTML content from the response
-    const $ = cheerio.load(html); // Loading the HTML content into Cheerio for easy DOM manipulation
+// Function to fetch and log followers count
+const fetchFollowersCount = () => {
+  // Making a GET request using Axios
+  axios
+    .get(url, { httpsAgent: proxyAgent }) // Sending a GET request to the Instagram profile URL using Axios and the specified proxy agent
+    .then((response) => {
+      // Parsing HTML content using Cheerio
+      const html = response.data; // Extracting the HTML content from the response
+      const $ = cheerio.load(html); // Loading the HTML content into Cheerio for easy DOM manipulation
 
-    // Extracting followers count from meta tags
-    const followersString = $('meta[property="og:description"]').attr('content'); // Finding the meta tag containing followers count
-    const match = followersString.match(/(\d+) Followers/); // Matching the followers count using a regular expression
-    const followersCount = match ? parseInt(match[1], 10) : null; // Extracting and converting the followers count to an integer
+      // Extracting followers count from meta tags
+      const followersString = $('meta[property="og:description"]').attr('content'); // Finding the meta tag containing followers count
+      const match = followersString.match(/(\d+) Followers/); // Matching the followers count using a regular expression
+      const followersCount = match ? parseInt(match[1], 10) : null; // Extracting and converting the followers count to an integer
 
-    // Logging the followers count
-    console.log('Followers count:', followersCount);
+      // Logging the followers count
+      console.log('Followers count:', followersCount);
 
-  })
-  .catch((error) => {
-    // Handling errors during the request
+    })
+    .catch((error) => {
+      // Handling errors during the request
 
-    // Log the specific error message
-    console.error('Error during the request:', error.message);
+      // Log the specific error message
+      console.error('Error during the request:', error.message);
 
-    // Checking if there is a response from the server
-    if (error.response) {
-      // Log the HTTP response status and data
-      console.error('Response status:', error.response.status);
-      console.error('Response data:', error.response.data);
-    }
+      // Checking if there is a response from the server
+      if (error.response) {
+        // Log the HTTP response status and data
+        console.error('Response status:', error.response.status);
+        console.error('Response data:', error.response.data);
+      }
 
-    // Log the full error object for further investigation
-    console.error('Full error:', error);
+      // Log the full error object for further investigation
+      console.error('Full error:', error);
+    });
+};
 
-  });
+// Fetch followers count every second
+setInterval(fetchFollowersCount, 1000);
+
